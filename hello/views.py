72b3af82adbd247b9699cadf59a30699c57e3f52
ahmadalizarei-fa <92165319+ahmadalizarei-fa@users.noreply.github.com>
2022-02-17 16:11:31 +0200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import requests
 
 
-
-def index(request):
+res = requests.get('http://www.york.ac.uk/teaching/cws/wws/webpage1.html')
+if res.status_code == 200: # check that the request went through
+  # print the entire html, should maintain internal newlines so that when it print to screen it isn't on a single line
+   return render (res.content)
+//def index(request):
    
-    return render(request, "index.html")
+  //  return render(request, "index.html")
 
