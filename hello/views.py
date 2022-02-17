18de4d7329c3bbc44daaecba5dae618c3022d5a3
ahmadalizarei-fa <92165319+ahmadalizarei-fa@users.noreply.github.com>
2022-02-17 16:14:00 +0200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -8,7 +8,7 @@ res = requests.get('http://www.york.ac.uk/teaching/cws/wws/webpage1.html')
 if res.status_code == 200: # check that the request went through
   # print the entire html, should maintain internal newlines so that when it print to screen it isn't on a single line
    return render (res.content)
-//def index(request):
+#def index(request):
    
-  //  return render(request, "index.html")
+  # return render(request, "index.html")
 
